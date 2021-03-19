7590e98c41a843fb8ee2332f5575bd64999600db
Cristian Tellez <cristellez.694@gmail.com>
2021-03-18 18:02:51 -0600

    commit rama2

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4';
-import {HeroContainer, HeroBg, VideoBg} from './HeroElements';
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements';
+import {Button} from '../ButtonElement';
+
+
+
 
 const HeroSection = () => {
+
+    
+        const [hover, setHover] = useState(false)
+
+        const onHover = () => {
+            setHover(!hover)
+        }
+    
+
+
     return (
        <HeroContainer>
            <HeroBg>
                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
            </HeroBg>
+           <HeroContent>
+               <HeroH1>Fuerza Automotriz Carmona</HeroH1>
+               <HeroP>
+                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, quisquam.
+               </HeroP>
+               <HeroBtnWrapper>
+                   <Button to='signup' onMouseEnter= {onHover} onMouseLeave={onHover}
+                   primary='true' dark='true'>
+                    Agenda una cita con nosotros {hover ? <ArrowForward /> :<ArrowRight/>} 
+                   </Button>
+               </HeroBtnWrapper>
+           </HeroContent>
        </HeroContainer>
     )
 }
